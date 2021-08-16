721765f422eb681327fa2d6c6842a29b7da4c712
Shubham Bhagat <bhagatshubham68@gmail.com>
2021-08-16 11:24:49 +0530

    Testing added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,42 +1,6 @@
-import express from 'express'
-import 'express-async-errors'
-import {json} from 'body-parser'
 import mongoose from 'mongoose'
-import cookieSession from 'cookie-session'
 
-
-import{ currentUserRouter } from './router/current-user'
-import{ signoutRouter} from './router/signout'
-import{ signinRouter } from './router/signin'
-import{ signupRouter } from './router/signup'
-import{ errorHandler } from './middleware/error-handler'
-import{ NotFoundError} from './errors/not-found-error'
-
-
-
-const app = express()
-app.set('trust proxy',true)
-app.use(json())
-app.use(
-	cookieSession({
-		signed:false,
-		secure: true
-	})
-)
-
-
-
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signupRouter)
-app.use(signoutRouter)
-
-app.get('*',()=>{
-	throw new NotFoundError()
-})
-
-app.use(errorHandler)
-
+import { app } from 'app'
 
 const start = async ()=>{
 
